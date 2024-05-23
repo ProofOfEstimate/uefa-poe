c4377df6fa1a70b9164badb5d4cd36eaa3119040
Thomas Gehrmann <s6thgehr@gmail.com>
2024-05-24 01:33:54 +0200

    Add dummy data and display it in a table

---
+++ lib/dummyData.ts
@@ -0,0 +1,51 @@
+type Match = {
+  id: string;
+  date: string;
+  teamA: string;
+  teamB: string;
+};
+
+export const matches: Match[] = [
+  {
+    id: "1",
+    date: "14.06.24 21:00",
+    teamA: "Germany",
+    teamB: "Scotland",
+  },
+  {
+    id: "2",
+    date: "15.06.24 15:00",
+    teamA: "Hungary",
+    teamB: "Switzerland",
+  },
+  {
+    id: "3",
+    date: "15.06.24 18:00",
+    teamA: "Spain",
+    teamB: "Croatia",
+  },
+  {
+    id: "4",
+    date: "15.06.24 21:00",
+    teamA: "Italy",
+    teamB: "Albania",
+  },
+  {
+    id: "5",
+    date: "16.06.24 15:00",
+    teamA: "Poland",
+    teamB: "Netherlands",
+  },
+  {
+    id: "6",
+    date: "16.06.24 18:00",
+    teamA: "Slovenia",
+    teamB: "Denmark",
+  },
+  {
+    id: "7",
+    date: "16.06.24 21:00",
+    teamA: "Serbia",
+    teamB: "England",
+  },
+];
