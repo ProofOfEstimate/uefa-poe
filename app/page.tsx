a554a0b470e4030da959e8b5af3c154eeb4f5c12
Thomas Gehrmann <s6thgehr@gmail.com>
2024-05-26 22:40:55 +0200

    Adding dummy data till final

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
@@ -9,13 +8,22 @@ import {
 } from "@/components/ui/table";
 import { Input } from "@/components/ui/input";
 
-import { matches } from "@/lib/dummyData";
+import {
+  matchesFinal,
+  matchesFirstMatchDay,
+  matchesQuarterFinals,
+  matchesRound16,
+  matchesSecondMatchday,
+  matchesSemiFinals,
+  matchesThirdMatchday,
+} from "@/lib/dummyData";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold mb-4">Matchday 1</h1>
       <Table className="min-w-[1000px]">
-        <TableCaption>Group phase</TableCaption>
         <TableHeader>
           <TableRow>
             <TableHead className="w-[100px]">Date</TableHead>
@@ -27,7 +35,7 @@ export default function Home() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {matches.map((match) => {
+          {matchesFirstMatchDay.map((match) => {
             return (
               <TableRow key={match.id}>
                 <TableCell className="font-medium">{match.date}</TableCell>
@@ -47,6 +55,211 @@ export default function Home() {
           })}
         </TableBody>
       </Table>
+      <Separator />
+      <h1 className="text-4xl font-bold mt-8">Matchday 2</h1>
+      <Table className="min-w-[1000px]">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Date</TableHead>
+            <TableHead>Team A</TableHead>
+            <TableHead>Team B</TableHead>
+            <TableHead>Prob. that Team A wins</TableHead>
+            <TableHead>Prob. that Team B wins</TableHead>
+            <TableHead>Prob. of a draw</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {matchesSecondMatchday.map((match) => {
+            return (
+              <TableRow key={match.id}>
+                <TableCell className="font-medium">{match.date}</TableCell>
+                <TableCell>{match.teamA}</TableCell>
+                <TableCell>{match.teamB}</TableCell>
+                <TableCell>
+                  <Input className="w-fit" type="number" min={0} max={100} />
+                </TableCell>
+                <TableCell>
+                  <Input className="w-fit" type="number" min={0} max={100} />
+                </TableCell>
+                <TableCell>
+                  <Input className="w-fit" type="number" min={0} max={100} />
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+      <Separator />
+      <h1 className="text-4xl font-bold mt-8">Matchday 3</h1>
+      <Table className="min-w-[1000px]">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Date</TableHead>
+            <TableHead>Team A</TableHead>
+            <TableHead>Team B</TableHead>
+            <TableHead>Prob. that Team A wins</TableHead>
+            <TableHead>Prob. that Team B wins</TableHead>
+            <TableHead>Prob. of a draw</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {matchesThirdMatchday.map((match) => {
+            return (
+              <TableRow key={match.id}>
+                <TableCell className="font-medium">{match.date}</TableCell>
+                <TableCell>{match.teamA}</TableCell>
+                <TableCell>{match.teamB}</TableCell>
+                <TableCell>
+                  <Input className="w-fit" type="number" min={0} max={100} />
+                </TableCell>
+                <TableCell>
+                  <Input className="w-fit" type="number" min={0} max={100} />
+                </TableCell>
+                <TableCell>
+                  <Input className="w-fit" type="number" min={0} max={100} />
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+      <Separator />
+      <h1 className="text-4xl font-bold mt-8">Round of 16</h1>
+      <Table className="min-w-[1000px]">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Date</TableHead>
+            <TableHead>Team A</TableHead>
+            <TableHead>Team B</TableHead>
+            <TableHead>Prob. that Team A wins</TableHead>
+            <TableHead>Prob. that Team B wins</TableHead>
+            <TableHead>Prob. of a draw</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {matchesRound16.map((match) => {
+            return (
+              <TableRow key={match.id}>
+                <TableCell className="font-medium">{match.date}</TableCell>
+                <TableCell>{match.teamA}</TableCell>
+                <TableCell>{match.teamB}</TableCell>
+                <TableCell>
+                  <Input className="w-fit" type="number" min={0} max={100} />
+                </TableCell>
+                <TableCell>
+                  <Input className="w-fit" type="number" min={0} max={100} />
+                </TableCell>
+                <TableCell>
+                  <Input className="w-fit" type="number" min={0} max={100} />
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+      <Separator />
+      <h1 className="text-4xl font-bold mt-8">Quarter Finals</h1>
+      <Table className="min-w-[1000px]">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Date</TableHead>
+            <TableHead>Team A</TableHead>
+            <TableHead>Team B</TableHead>
+            <TableHead>Prob. that Team A wins</TableHead>
+            <TableHead>Prob. that Team B wins</TableHead>
+            <TableHead>Prob. of a draw</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {matchesQuarterFinals.map((match) => {
+            return (
+              <TableRow key={match.id}>
+                <TableCell className="font-medium">{match.date}</TableCell>
+                <TableCell>{match.teamA}</TableCell>
+                <TableCell>{match.teamB}</TableCell>
+                <TableCell>
+                  <Input className="w-fit" type="number" min={0} max={100} />
+                </TableCell>
+                <TableCell>
+                  <Input className="w-fit" type="number" min={0} max={100} />
+                </TableCell>
+                <TableCell>
+                  <Input className="w-fit" type="number" min={0} max={100} />
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+      <Separator />
+      <h1 className="text-4xl font-bold mt-8">Semi Finals</h1>
+      <Table className="min-w-[1000px]">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Date</TableHead>
+            <TableHead>Team A</TableHead>
+            <TableHead>Team B</TableHead>
+            <TableHead>Prob. that Team A wins</TableHead>
+            <TableHead>Prob. that Team B wins</TableHead>
+            <TableHead>Prob. of a draw</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {matchesSemiFinals.map((match) => {
+            return (
+              <TableRow key={match.id}>
+                <TableCell className="font-medium">{match.date}</TableCell>
+                <TableCell>{match.teamA}</TableCell>
+                <TableCell>{match.teamB}</TableCell>
+                <TableCell>
+                  <Input className="w-fit" type="number" min={0} max={100} />
+                </TableCell>
+                <TableCell>
+                  <Input className="w-fit" type="number" min={0} max={100} />
+                </TableCell>
+                <TableCell>
+                  <Input className="w-fit" type="number" min={0} max={100} />
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+      <Separator />
+      <h1 className="text-4xl font-bold mt-8">Final</h1>
+      <Table className="min-w-[1000px]">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Date</TableHead>
+            <TableHead>Team A</TableHead>
+            <TableHead>Team B</TableHead>
+            <TableHead>Prob. that Team A wins</TableHead>
+            <TableHead>Prob. that Team B wins</TableHead>
+            <TableHead>Prob. of a draw</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {matchesFinal.map((match) => {
+            return (
+              <TableRow key={match.id}>
+                <TableCell className="font-medium">{match.date}</TableCell>
+                <TableCell>{match.teamA}</TableCell>
+                <TableCell>{match.teamB}</TableCell>
+                <TableCell>
+                  <Input className="w-fit" type="number" min={0} max={100} />
+                </TableCell>
+                <TableCell>
+                  <Input className="w-fit" type="number" min={0} max={100} />
+                </TableCell>
+                <TableCell>
+                  <Input className="w-fit" type="number" min={0} max={100} />
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+      <Separator />
     </main>
   );
 }
--- lib/dummyData.ts
+++ lib/dummyData.ts
@@ -5,7 +5,7 @@ type Match = {
   teamB: string;
 };
 
-export const matches: Match[] = [
+export const matchesFirstMatchDay: Match[] = [
   {
     id: "1",
     date: "14.06.24 21:00",
@@ -47,5 +47,284 @@ export const matches: Match[] = [
     date: "16.06.24 21:00",
     teamA: "Serbia",
     teamB: "England",
+  },
+  {
+    id: "8",
+    date: "17.06.24 15:00",
+    teamA: "Romania",
+    teamB: "Ukraine",
+  },
+  {
+    id: "9",
+    date: "17.06.24 18:00",
+    teamA: "Belgium",
+    teamB: "Slovakia",
+  },
+  {
+    id: "10",
+    date: "17.06.24 21:00",
+    teamA: "Austria",
+    teamB: "France",
+  },
+  {
+    id: "11",
+    date: "18.06.24 18:00",
+    teamA: "Türkiye",
+    teamB: "Georgia",
+  },
+  {
+    id: "12",
+    date: "18.06.24 21:00",
+    teamA: "Portugal",
+    teamB: "Czechia",
+  },
+];
+
+export const matchesSecondMatchday: Match[] = [
+  {
+    id: "13",
+    date: "19.06.24 15:00",
+    teamA: "Croatia",
+    teamB: "Albania",
+  },
+  {
+    id: "14",
+    date: "19.06.24 18:00",
+    teamA: "Germany",
+    teamB: "Hungary",
+  },
+  {
+    id: "15",
+    date: "19.06.24 21:00",
+    teamA: "Scotland",
+    teamB: "Switzerland",
+  },
+  {
+    id: "16",
+    date: "20.06.24 15:00",
+    teamA: "Slovenia",
+    teamB: "Serbia",
+  },
+  {
+    id: "17",
+    date: "20.06.24 18:00",
+    teamA: "Denmark",
+    teamB: "England",
+  },
+  {
+    id: "18",
+    date: "20.06.24 21:00",
+    teamA: "Spain",
+    teamB: "Italy",
+  },
+  {
+    id: "19",
+    date: "21.06.24 15:00",
+    teamA: "Slovakia",
+    teamB: "Ukraine",
+  },
+  {
+    id: "20",
+    date: "21.06.24 18:00",
+    teamA: "Poland",
+    teamB: "Austria",
+  },
+  {
+    id: "21",
+    date: "21.06.24 21:00",
+    teamA: "Netherland",
+    teamB: "France",
+  },
+  {
+    id: "22",
+    date: "22.06.24 15:00",
+    teamA: "Georgia",
+    teamB: "Czechia",
+  },
+  {
+    id: "23",
+    date: "22.06.24 18:00",
+    teamA: "Türkiye",
+    teamB: "Portugal",
+  },
+  {
+    id: "24",
+    date: "22.06.24 21:00",
+    teamA: "Belgium",
+    teamB: "Romania",
+  },
+];
+
+export const matchesThirdMatchday: Match[] = [
+  {
+    id: "25",
+    date: "23.06.24 21:00",
+    teamA: "Switzerland",
+    teamB: "Germany",
+  },
+  {
+    id: "26",
+    date: "23.06.24 21:00",
+    teamA: "Scotland",
+    teamB: "Hungary",
+  },
+  {
+    id: "27",
+    date: "24.06.24 21:00",
+    teamA: "Albania",
+    teamB: "Spain",
+  },
+  {
+    id: "28",
+    date: "24.06.24 21:00",
+    teamA: "Croatia",
+    teamB: "Italy",
+  },
+  {
+    id: "29",
+    date: "25.06.24 18:00",
+    teamA: "Netherlands",
+    teamB: "Austria",
+  },
+  {
+    id: "30",
+    date: "25.06.24 18:00",
+    teamA: "France",
+    teamB: "Poland",
+  },
+  {
+    id: "31",
+    date: "25.06.24 21:00",
+    teamA: "England",
+    teamB: "Slovenia",
+  },
+  {
+    id: "32",
+    date: "25.06.24 21:00",
+    teamA: "Denmark",
+    teamB: "Serbia",
+  },
+  {
+    id: "33",
+    date: "26.06.24 18:00",
+    teamA: "Slovakia",
+    teamB: "Romania",
+  },
+  {
+    id: "34",
+    date: "26.06.24 18:00",
+    teamA: "Ukraine",
+    teamB: "Belgium",
+  },
+  {
+    id: "35",
+    date: "26.06.24 21:00",
+    teamA: "Georgia",
+    teamB: "Portugal",
+  },
+  {
+    id: "36",
+    date: "26.06.24 21:00",
+    teamA: "Czechia",
+    teamB: "Türkiye",
+  },
+];
+
+export const matchesRound16: Match[] = [
+  {
+    id: "37",
+    date: "29.06.24 18:00",
+    teamA: "tbd",
+    teamB: "tbd",
+  },
+  {
+    id: "38",
+    date: "29.06.24 21:00",
+    teamA: "tbd",
+    teamB: "tbd",
+  },
+  {
+    id: "39",
+    date: "30.06.24 18:00",
+    teamA: "tbd",
+    teamB: "tbd",
+  },
+  {
+    id: "40",
+    date: "30.06.24 21:00",
+    teamA: "tbd",
+    teamB: "tbd",
+  },
+  {
+    id: "41",
+    date: "01.07.24 18:00",
+    teamA: "tbd",
+    teamB: "tbd",
+  },
+  {
+    id: "42",
+    date: "01.07.24 21:00",
+    teamA: "tbd",
+    teamB: "tbd",
+  },
+  {
+    id: "43",
+    date: "02.07.24 18:00",
+    teamA: "tbd",
+    teamB: "tbd",
+  },
+  {
+    id: "44",
+    date: "02.07.24 21:00",
+    teamA: "tbd",
+    teamB: "tbd",
+  },
+];
+export const matchesQuarterFinals: Match[] = [
+  {
+    id: "45",
+    date: "05.07.24 18:00",
+    teamA: "tbd",
+    teamB: "tbd",
+  },
+  {
+    id: "46",
+    date: "05.07.24 21:00",
+    teamA: "tbd",
+    teamB: "tbd",
+  },
+  {
+    id: "47",
+    date: "06.07.24 18:00",
+    teamA: "tbd",
+    teamB: "tbd",
+  },
+  {
+    id: "48",
+    date: "07.07.24 21:00",
+    teamA: "tbd",
+    teamB: "tbd",
+  },
+];
+export const matchesSemiFinals: Match[] = [
+  {
+    id: "49",
+    date: "09.07.24 21:00",
+    teamA: "tbd",
+    teamB: "tbd",
+  },
+  {
+    id: "50",
+    date: "10.07.24 21:00",
+    teamA: "tbd",
+    teamB: "tbd",
+  },
+];
+export const matchesFinal: Match[] = [
+  {
+    id: "51",
+    date: "14.07.24 21:00",
+    teamA: "tbd",
+    teamB: "tbd",
   },
 ];
