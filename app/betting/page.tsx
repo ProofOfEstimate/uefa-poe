521fba3b677dd6ccd9d2ebe58f428157e9bbd8b5
Thomas Gehrmann <s6thgehr@gmail.com>
2024-06-05 21:05:47 +0200

    Add quick tour carousel

--- app/betting/page.tsx
+++ app/betting/page.tsx
@@ -10,6 +10,22 @@ import {
 import { Separator } from "@/components/ui/separator";
 import SideNav from "@/components/sidenav";
 import { MatchDay } from "@/components/match-day";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Button } from "@/components/ui/button";
 
 const Betting = () => (
   <div>
@@ -22,6 +38,56 @@ const Betting = () => (
         <SideNav />
       </aside>
       <main className="flex w-full min-h-screen flex-col items-center justify-between p-4 sm:p-24">
+        <Dialog>
+          <DialogTrigger>
+            <Button asChild>
+              <div>Quick Tour</div>
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <Carousel className="mx-8">
+              <CarouselContent>
+                <CarouselItem>
+                  <div className="flex flex-col gap-4">
+                    <div className="text-lg md:text-xl font-bold">
+                      Predict Probabilities, not just outcomes!
+                    </div>
+                    <div>
+                      With Poe, you predict how likely something is to happen.
+                      Your betting stake goes into a pool. Poe uses a special
+                      system to score your forecast and determine your payout.
+                    </div>
+                  </div>
+                </CarouselItem>
+                <CarouselItem>
+                  <div className="flex flex-col gap-4">
+                    <div className="text-lg md:text-xl font-bold">
+                      Always in the Game!
+                    </div>
+                    <div>
+                      Update your beliefs as you learn more and the match
+                      progresses. Poe will calculate a time-averaged score after
+                      the end of the match which determines your payout.
+                    </div>
+                  </div>
+                </CarouselItem>
+                <CarouselItem>
+                  <div className="flex flex-col gap-4">
+                    <div className="text-lg md:text-xl font-bold">
+                      Get rewarded for your insights!
+                    </div>
+                    <div>
+                      If you are right, you make a profit in expectation.
+                    </div>
+                  </div>
+                </CarouselItem>
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </DialogContent>
+        </Dialog>
+
         <MatchDay
           id="matchday1"
           title="Matchday 1"
