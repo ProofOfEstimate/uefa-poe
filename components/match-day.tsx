6139b5c1e3f07b9a77a9d44421ce410526f534aa
Thomas Gehrmann <s6thgehr@gmail.com>
2024-06-17 18:35:30 +0200

    Add toggle to Matchdays

--- components/match-day.tsx
+++ components/match-day.tsx
@@ -1,5 +1,9 @@
+"use client";
 import { Match } from "@/lib/dummyData";
 import { MatchCard } from "./match-card";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { ChevronDownIcon, ChevronUpIcon } from "@radix-ui/react-icons";
 
 export const MatchDay = ({
   id,
@@ -9,15 +13,31 @@ export const MatchDay = ({
   id: string;
   title: string;
   matches: Match[];
-}) => (
-  <div className="mb-8">
-    <h2 id={id} className="text-4xl font-bold mb-8 text-center">
-      {title}
-    </h2>
-    <div className="flex flex-wrap justify-center gap-5 md:gap-x-10">
-      {matches.map((match: Match) => (
-        <MatchCard key={match.id} match={match} />
-      ))}
+}) => {
+  const [isVisible, setIsVisible] = useState(true);
+  return (
+    <div className="mb-8">
+      <div className="flex gap-4 items-center mb-8 justify-center">
+        <h2 id={id} className="text-4xl font-bold text-center">
+          {title}
+        </h2>
+        <Button
+          size={"icon"}
+          variant={"outline"}
+          onClick={() => {
+            setIsVisible(!isVisible);
+          }}
+        >
+          {isVisible ? <ChevronUpIcon /> : <ChevronDownIcon />}
+        </Button>
+      </div>
+      {isVisible && (
+        <div className={`flex flex-wrap justify-center gap-5 md:gap-x-10`}>
+          {matches.map((match: Match) => (
+            <MatchCard key={match.id} match={match} />
+          ))}
+        </div>
+      )}
     </div>
-  </div>
-);
+  );
+};
